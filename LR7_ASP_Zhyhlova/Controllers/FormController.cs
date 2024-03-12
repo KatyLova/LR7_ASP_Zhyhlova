aa11e7d534725be76cd3e7a54323c40ad3edbb44
KatyLova <katy.lova803@gmail.com>
2024-03-12 23:08:32 +0200

    New project to push to GitHub

---
+++ LR7_ASP_Zhyhlova/Controllers/FormController.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using LR7_ASP_Zhyhlova.Models;
+
+
+namespace LR7_ASP_Zhyhlova.Controllers
+{
+    public class FormsController : Controller
+    {
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile([FromForm] FormInformation formInformation)
+        {
+            var text = JsonSerializer.Serialize(new
+            {
+                formInformation.FirstName,
+                formInformation.Surname
+            }, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+            return File(stream, "application/txt", $"{formInformation.FileName}.txt");
+        }
+    }
+}
---
+++ LR7_ASP_Zhyhlova/Models/FormInformation.cs
@@ -0,0 +1,11 @@
+﻿namespace LR7_ASP_Zhyhlova.Models
+{
+    public class FormInformation
+    {
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string FileName { get; set; }
+    }
+}
